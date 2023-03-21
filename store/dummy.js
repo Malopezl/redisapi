bb56c845c93646b0f71adc809400f3069d6d7ac5
Marvin Lopez <lopez.marvin9@gmail.com>
2023-03-20 23:51:14 -0600

    add routes to the api

--- store/dummy.js
+++ store/dummy.js
@@ -1,26 +1,26 @@
 const db = {
   user: [
     {
-      id: 1,
+      id: "1",
       name: "Carlos",
     },
   ],
 };
 
-function list(table) {
+async function list(table) {
   return db[table];
 }
 
-function get(table, id) {
-  let col = list(table);
+async function get(table, id) {
+  let col = await list(table);
   return col.filter((item) => item.id === id)[0] || null;
 }
 
-function upsert(table, data) {
+async function upsert(table, data) {
   db[table].push(data);
 }
 
-function remove(table, id) {
+async function remove(table, id) {
   return true;
 }
 
