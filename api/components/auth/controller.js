c7f776d5026e4b513e08421c3b96d626968e56e5
Marvin Lopez <lopez.marvin9@gmail.com>
2023-03-29 23:32:26 -0600

    add password encryption

--- api/components/auth/controller.js
+++ api/components/auth/controller.js
@@ -1,5 +1,6 @@
-const auth = require("../../../auth");
+const bcrypt = require("bcrypt");
 
+const auth = require("../../../auth");
 const TABLE = "auth";
 
 module.exports = function (injectedStore) {
@@ -10,16 +11,18 @@ module.exports = function (injectedStore) {
 
   async function login(username, password) {
     const data = await store.query(TABLE, { username: username });
-    if (data.password === password) {
-      //generate token
-      delete data.password;
-      return auth.sign(data);
-    } else {
-      throw new Error("Informacion invalida");
-    }
+
+    return bcrypt.compare(password, data.password).then((sonIguales) => {
+      if (sonIguales) {
+        /* Generate token */
+        return auth.sign(data);
+      } else {
+        throw new Error("Informacion invalida");
+      }
+    });
   }
 
-  function upsert(data) {
+  async function upsert(data) {
     const authData = {
       id: data.id,
     };
@@ -29,7 +32,7 @@ module.exports = function (injectedStore) {
     }
 
     if (data.password) {
-      authData.password = data.password;
+      authData.password = await bcrypt.hash(data.password, 10);
     }
 
     return store.upsert(TABLE, authData);
